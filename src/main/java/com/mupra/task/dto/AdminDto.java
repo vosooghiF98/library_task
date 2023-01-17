ce63f5bac702d2845c050e619e3daade363eb8e2
farshid vosooghi <vosooghifarshid95@gmail.com>
2023-01-17 11:32:25 +0330

    create AdminDto class

---
+++ src/main/java/com/mupra/task/dto/AdminDto.java
@@ -0,0 +1,14 @@
+package com.mupra.task.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AdminDto {
+    String username;
+    String password;
+}
