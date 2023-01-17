4d6e281ff50758f4e344c518a046beb0d64e8108
farshid vosooghi <vosooghifarshid95@gmail.com>
2023-01-17 11:28:39 +0330

    create PublisherService interface

---
+++ src/main/java/com/mupra/task/service/PublisherService.java
@@ -0,0 +1,7 @@
+package com.mupra.task.service;
+
+import com.mupra.task.base.service.BaseService;
+import com.mupra.task.entity.Publisher;
+
+public interface PublisherService extends BaseService<Publisher> {
+}
