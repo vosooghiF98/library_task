d5cb93766dba3953d83f73bcc2e3b65ece8f7ea9
farshid vosooghi <vosooghifarshid95@gmail.com>
2023-01-17 11:33:12 +0330

    create WriterDto class

---
+++ src/main/java/com/mupra/task/dto/WriterDto.java
@@ -0,0 +1,16 @@
+package com.mupra.task.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class WriterDto {
+    String name;
+    LocalDate birthday;
+}
