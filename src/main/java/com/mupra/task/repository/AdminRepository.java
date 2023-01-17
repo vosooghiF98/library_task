cc4befa64c813d082fbda8719aacf6bbf7985df2
farshid vosooghi <vosooghifarshid95@gmail.com>
2023-01-17 11:26:06 +0330

    create AdminRepository interface

---
+++ src/main/java/com/mupra/task/repository/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.mupra.task.repository;
+
+import com.mupra.task.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByUsername(String username);
+}
