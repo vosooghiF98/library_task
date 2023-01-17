e03500353f37a15bd078517a90e6780b8dd5873b
farshid vosooghi <vosooghifarshid95@gmail.com>
2023-01-17 11:24:16 +0330

    create Admin class

---
+++ src/main/java/com/mupra/task/entity/Admin.java
@@ -0,0 +1,57 @@
+package com.mupra.task.entity;
+
+import com.mupra.task.base.entity.BaseEntity;
+import com.mupra.task.enums.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Admin extends BaseEntity implements UserDetails {
+    @Column(unique = true,nullable = false)
+    String username;
+    @Column(nullable = false)
+    String password;
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(getRole().name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
